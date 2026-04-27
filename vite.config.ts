80eeca45b28484bd2816d69964d506d268f89790
m-barazi <121522068+m-barazi@users.noreply.github.com>
2026-04-27 17:29:03 +0200

    Add allowedHosts to preview in vite.config.ts
    Add allowedHosts to preview configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['calculator.barazi.cloud'],
+  },
+   preview: {
+   allowedHosts: ['calculator.barazi.cloud'],
   },
   build: {
     outDir: 'dist',
