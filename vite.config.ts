16dd74259359a660f779bb4e115d14ad6cd7497b
m-barazi <121522068+m-barazi@users.noreply.github.com>
2026-04-27 17:59:05 +0200

    Change allowedHosts to 'all' in preview config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   preview: {
     host: true,
     port: 4173,
-    allowedHosts: ['calculator.barazi.cloud'],
+    allowedHosts: 'all',
   },
   build: {
     outDir: 'dist',
